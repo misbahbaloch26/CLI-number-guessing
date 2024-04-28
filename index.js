6a6f1e126d73bd2ee79c5215642e7cd78da68146
misbahbaloch26 <162510202+misbahbaloch26@users.noreply.github.com>
2024-04-28 14:02:29 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+import inquirer from "inquirer";
+const randomnumber = Math.floor(Math.random() * 10 + 1);
+const answer = await inquirer.prompt([
+    { name: "userguessednumber",
+        type: "number",
+        message: "please guess a number between 1-10",
+    }
+]);
+console.log(answer);
+//conditional statment
+if (answer.userguessednumber === randomnumber) {
+    console.log("Congtratulations! you guessed right number.");
+}
+else {
+    console.log("Sorry! you guessed wrong number.");
+}
